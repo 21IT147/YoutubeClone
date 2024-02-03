6b1b9d634f0553369e0318cd7523a8766dde72a1
21IT147 <21IT147@charusat.edu.in>
2024-02-03 15:27:18 +0530

    DB Completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 // require('dotenv').config({path:"./.env"});  {This also Works Perfectly}
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 dotenv.config({
     path:"./.env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`Error : ${error}`)
+        process.exit(1);
+    })
+    app.listen(process.env.PORT || 8080,()=>{
+        console.log(`App is running on port no. ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log(`Error connecting to MongoDB Database !! ${error}`)
+})
 
 /*
 Method 1: // Chaotic Method 
