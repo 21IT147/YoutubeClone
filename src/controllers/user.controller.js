9cd8952277a6cf45f64e300902fa638f64016e5d
21IT147 <21IT147@charusat.edu.in>
2024-02-06 17:53:12 +0530

    auth middelware and logout route completed part-1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -147,4 +147,34 @@ const loginUser = asyncHandler(async (req,res)=>{
     )
 })
 
-export { registerUser, loginUser };
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set :{
+                refreshToken : undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken")
+    .clearCookie("refreshToken")
+    .json(new ApiResponse(
+        200,
+        {},
+        "User LoggedOut Successfully"
+    ))
+})
+
+export { registerUser, loginUser, logoutUser };
